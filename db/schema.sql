fa9180ab5c1fb60a267a862c7a91c91eee3f7a30
21ghosts <nathanmyoung44@gmai.com>
2018-04-03 13:25:20 -0400

    final commit

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE burgers
 (
     id INT(10) AUTO_INCREMENT NOT NULL,
     burgers_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT "0",
+    devoured BOOLEAN NOT NULL DEFAULT false,,
     date TIMESTAMP,
     PRIMARY KEY (id)
 );
--- db/server.js
+++
@@ -1,2 +0,0 @@
-var express = require
-var bodyParser = require
